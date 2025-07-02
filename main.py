6f8238c2e378be7b0e24418aa4143505d0a38010
FrostyRaptor <e1wink@outlook.com>
2025-07-02 12:16:44 -0500

    Fix a lot

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-model = 'gemini-2.0-flash-001'
-content = sys.argv[1]
+def main():
+    # Get and store the api key from .env
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-messages = [
-    types.Content(role="user", parts=[types.Part(text=content)])
-]
+    # Create instance of Gemini client
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model=model, contents=messages)
+    # Gemini model to use
+    model = "gemini-2.0-flash-001"
 
-print(response.text)
-print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    # Content to send the model
+    content = sys.argv[1]
+
+    # Send variables to response function
+    get_response(client, model, content)
+
+def get_response(client, model, content):
+    # Send content and generate reponse
+    response = client.models.generate_content(model=model, contents=content)
+
+    print(response.text)
+    print(response.usage_metadata.prompt_token_count)
+    print(response.usage_metadata.candidates_token_count)
+
+if __name__ == "__main__":
+    main()
