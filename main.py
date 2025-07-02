63184eb232edc5d2c86561da346be4af70df8c9e
FrostyRaptor <e1wink@outlook.com>
2025-07-02 18:43:45 -0500

    Added the calculator directory

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     # Get and store the api key from .env
@@ -15,18 +16,32 @@ def main():
     model = "gemini-2.0-flash-001"
 
     # Content to send the model
-    content = sys.argv[1]
+    prompt = sys.argv[1]
+
+    # Checks if verbose was sent
+    verbose = "--verbose" in sys.argv
+
+    # Store messages for later use
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
+    # Send back the prompt to user if verbose
+    if verbose:
+        print(f'User Prompt: {prompt}')
 
     # Send variables to response function
-    get_response(client, model, content)
+    get_response(client, model, messages, verbose)
 
-def get_response(client, model, content):
+def get_response(client, model, messages, verbose):
     # Send content and generate reponse
-    response = client.models.generate_content(model=model, contents=content)
+    response = client.models.generate_content(model=model, contents=messages)
 
-    print(response.text)
-    print(response.usage_metadata.prompt_token_count)
-    print(response.usage_metadata.candidates_token_count)
+    # Print extra data if verbose
+    if verbose:
+        print(f'Prompt Token Count: {response.usage_metadata.prompt_token_count}')
+        print(f'Candidates Token Count: {response.usage_metadata.candidates_token_count}')
+    
+    # Always print no matter what
+    print(f'\nResponse: \n\n{response.text}')
 
 if __name__ == "__main__":
     main()
